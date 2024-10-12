2468263554a69150d1a4426638e5100fea465022
mahmood-meesho <mahmood.ahmad@meesho.com>
2024-10-13 00:19:43 +0530

    Allocating more memory for gradle build

--- shared/src/17Main/kotlin/com/weather/forecastify/shared/Platform.17.kt
+++ shared/src/17Main/kotlin/com/weather/forecastify/shared/Platform.17.kt
@@ -1,3 +1,8 @@
 package com.weather.forecastify.shared
 
-actual fun getPlatform(): Platform
+actual fun getPlatform(): Platform {
+    return object : Platform {
+        override val name: String
+            get() = "Some platform name"
+    }
+}
