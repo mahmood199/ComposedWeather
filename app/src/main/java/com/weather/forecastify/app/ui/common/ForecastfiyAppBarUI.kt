e3591667b181d11f7f4265498efd7efdd089ddea
mahmood-meesho <gaudentas199@gmail.com>
2024-11-02 20:26:54 +0530

    Added custom side drawer for better UX

--- app/src/main/java/com/weather/forecastify/app/ui/common/ForecastfiyAppBarUI.kt
+++ app/src/main/java/com/weather/forecastify/app/ui/common/ForecastfiyAppBarUI.kt
@@ -1,7 +1,12 @@
 package com.weather.forecastify.app.ui.common
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,7 +27,8 @@ fun ForecastfiyAppBarUI(
     title: String,
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier,
-    backButtonIcon: ImageVector? = null
+    backButtonIcon: ImageVector? = null,
+    onAction: () -> Unit = {}
 ) {
     TopAppBar(
         title = {
@@ -41,6 +47,13 @@ fun ForecastfiyAppBarUI(
                 Text(text = title)
             }
         },
+        actions = {
+            Image(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Navigate to Search",
+                modifier = Modifier.clickable(onClick = onAction)
+            )
+        },
         modifier = modifier
             .zIndex(20f)
     )
@@ -53,7 +66,8 @@ fun ForecastifyAppBarUIPreview() {
         ForecastfiyAppBarUI(
             title = "Home",
             backButtonIcon = null,
-            onBackPressed = {}
+            onBackPressed = {},
+            onAction = {}
         )
     }
 }
