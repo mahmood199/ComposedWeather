c881666fd70adda05a4495a92d46b21ba0119bfe
Mahmood <gaudentas199@gmail.com>
2024-11-02 15:08:31 +0530

    Draft commit for adding detekt to project (#10)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,12 @@ pluginManagement {
         maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
     }
+    plugins {
+        kotlin("jvm") version "2.0.21"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
